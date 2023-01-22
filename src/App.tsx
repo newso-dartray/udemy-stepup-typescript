3eda4d096fc53e7995ef0ebbfffa8a9aa4737099
paperStudent <newso.dartray@gmail.com>
2023-01-22 19:34:45 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+import { useState } from "react";
 import './App.css';
+import { Practice1 } from './practices/Practice1';
+import { Practice2 } from './practices/Practice2';
+import { Practice3 } from './practices/Practice3';
+import { Practice4 } from './practices/Practice4';
+import { Todo } from './Todo';
+import { TodoType } from "./types/todo";
+import { Text } from "./Text";
+import { UserProfile } from './UserProfile';
+import { User } from './types/user';
+
+const user: User = {
+	name: "niwa",
+	hobbies: ["映画", "音楽"]
+};
 
 function App() {
+	const [todos, setTodos] = useState<Array<TodoType>>([]);
+
+	const onClickFetchData = () => {
+		axios
+			.get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos")
+			.then((res) => {
+				setTodos(res.data);
+			});
+	};
+	
+	const SButton: object = {
+		marginTop: "60px"
+	};
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+				<Practice1 />
+				<Practice2 />
+				<Practice3 />
+				<Practice4 />
+				<UserProfile user={user}/>
+				<Text color="red" fontSize="18px"/>
+				<button onClick={onClickFetchData} style={SButton}>データ取得</button>
+				{todos.map((todo) => (
+					<Todo 
+						key={todo.id}
+						title={todo.title} 
+						userId={todo.userId} 
+						completed={todo.completed}
+					/>
+				))} 
     </div>
   );
 }
---
+++ src/Todo.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+import { TodoType } from "./types/todo";
+
+export const Todo: FC<Omit<TodoType, "id">> = (props) => {
+	const {title, userId, completed = false} = props;
+	const completeMark = completed ? "[完了]" : "[未完了]" ;
+	return (
+		<p>
+			{`${completeMark} ${title}(ユーザー:${userId})`}
+		</p>
+	);
+}
